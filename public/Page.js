04f1f5baff5e6b5a4a17af136ea7f80e92f97ba5
Jordan Stone <jstone17@terpmail.umd.edu>
2026-05-14 01:38:06 -0400

    added files

--- public/Page.js
+++ public/Page.js
@@ -1,82 +1,13 @@
-const form = document.getElementById("lostPetForm");
+let map;
+function loadMap(){
+    map = L.map('map').setView([32, -95], 6);
 
-if (form) {
-
-    form.addEventListener("submit", async (event) => {
-
-        event.preventDefault();
-
-        const imageFile = document.getElementById("imageUpload").files[0];
-
-        let imageBase64 = "";
-
-        if (imageFile) {
-
-            const reader = new FileReader();
-
-            reader.onloadend = async () => {
-
-                imageBase64 = reader.result;
-
-                submitPet(imageBase64);
-
-            };
-
-            reader.readAsDataURL(imageFile);
-
-        } 
-        else {
-
-            submitPet("");
-
-        }
-
-    });
-
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    findPets(pets);
 }
 
-async function submitPet(imageData) {
-
-    const petData = {
-
-        animal_type: document.getElementById("animalType").value,
-        breed: document.getElementById("breed").value,
-        status: "Lost",
-        description: document.getElementById("description").value,
-        color: document.getElementById("color").value,
-        last_seen_location: document.getElementById("location").value,
-        contact_information: document.getElementById("contact").value,
-        image: imageData
-
-    };
-
-    console.log("SENDING:", petData);
-
-    try {
-
-        const response = await fetch("/pets", {
-
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(petData)
-
-        });
-
-        const data = await response.json();
-
-        console.log("SERVER RESPONSE:", data);
-
-        alert("Pet submitted!");
-
-    } 
-    catch (error) {
-
-        console.log("ERROR:", error);
-
-        alert("Submission failed");
-
-    }
-
-}
+window.onload = loadMap;
